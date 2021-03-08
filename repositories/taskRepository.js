1d22a6197ed30f4ba634d3639b3431a615f7ca04
Manuel Vera <manuelvera202@gmail.com>
2021-03-07 23:01:41 -0300

    added completeTask

--- repositories/taskRepository.js
+++ repositories/taskRepository.js
@@ -1,44 +1,50 @@
-const Task = require('../models/task');
-const { getData, saveData } = require('../helpers/fileManager');
+const Task = require("../models/Task");
+const { getData, saveData } = require("../helpers/fileManager");
 
 class TaskRepository {
+  _tasks = null;
 
-    _tasks = null;
+  constructor() {
+    const data = getData();
 
-    constructor() {
-
-        const data = getData();
-
-        if (data) {
-            this._tasks = data;
-        } else {
-            this._tasks = [];
-        }
+    if (data) {
+      this._tasks = data;
+    } else {
+      this._tasks = [];
     }
+  }
 
-    getAllTasks() {
-        return this._tasks.map(task => { return { title: task.title, done: task.done, finished: task.finished } });
-    }
+  getAllTasks() {
+    return this._tasks.map((task) => {
+      return { title: task.title, done: task.done, finished: task.finished };
+    });
+  }
 
-    /**
-     * 
-     * @param {String} title 
-     */
+  /**
+   *
+   * @param {String} title
+   */
 
-    createTask(title) {
-        const task = new Task(title);
-        this._tasks.push(task);
-        saveData(this._tasks);
-    }
+  createTask(title) {
+    const task = new Task(title);
+    this._tasks.push(task);
+    saveData(this._tasks);
+  }
 
-    deleteTask(title) {
-        this._tasks = this._tasks.filter(task => task.title !== title);
-        saveData(this._tasks);
-    }
+  deleteTask(title) {
+    this._tasks = this._tasks.filter((task) => task.title !== title);
+    saveData(this._tasks);
+  }
 
-    completeTask() {
-
-    }
+  completeTask(title) {
+    this._tasks.map((task) => {
+      if (task.title === title) {
+        task.done = true;
+        task.finished = new Date();
+      }
+    });
+    saveData(this._tasks);
+  }
 }
 
 module.exports = TaskRepository;
