d556a4966d7d948be90a840122f135691944a33a
Manuel Vera <manuelvera202@gmail.com>
2021-03-11 13:08:26 -0300

    merge filemanager

--- helpers/fileManager.js
+++ helpers/fileManager.js
@@ -20,11 +20,4 @@ const saveData = (data) => {
   fs.writeFileSync(name, JSON.stringify(data));
 };
 
-<<<<<<< HEAD
 module.exports = { getData, saveData };
-=======
-    fs.writeFileSync(name, JSON.stringify(data));
-}
-
-module.exports = { getData, saveData }
->>>>>>> cd84c79fa4ee2141aa8b0eac9082ab8427277b29
